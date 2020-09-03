caeb488e005da66189eb0ba0bb451cfcb6f6fc69
Welcome17 <67475009+Welcome17@users.noreply.github.com>
2020-09-03 15:29:06 +0400

    Add files via upload

---
+++ precache-manifest.45d3c75893ec5fe7d671987194f6c028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4788d27285995a5a91d6b92e9e36f8f0",
+    "url": "/spaziovip/index.html"
+  },
+  {
+    "revision": "89783ffb616a08a44f1c",
+    "url": "/spaziovip/static/css/2.bc3f430c.chunk.css"
+  },
+  {
+    "revision": "7a411bc1a2f7f64c9a99",
+    "url": "/spaziovip/static/css/main.e0f56a58.chunk.css"
+  },
+  {
+    "revision": "89783ffb616a08a44f1c",
+    "url": "/spaziovip/static/js/2.f959e2c9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/spaziovip/static/js/2.f959e2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a411bc1a2f7f64c9a99",
+    "url": "/spaziovip/static/js/main.9f18576f.chunk.js"
+  },
+  {
+    "revision": "fc6c0d79038184fe2817",
+    "url": "/spaziovip/static/js/runtime-main.df40f563.js"
+  }
+]);
